963e3480850699b550246728d49cb3ae1adca7f8
mingming.ye <mingming.ye@upai.com>
2016-07-18 14:24:04 +0800

    adjust speed

--- upplayer/src/main/java/com/upyun/upplayer/widget/UpVideoView.java
+++ upplayer/src/main/java/com/upyun/upplayer/widget/UpVideoView.java
@@ -292,7 +292,7 @@ public class UpVideoView extends FrameLayout implements MediaController.MediaPla
             mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "http-detect-range-support", 0);
             mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_CODEC, "skip_loop_filter", 48);
             mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", "4096");
-            mMediaPlayer.setSpeed(1.03f);
+            mMediaPlayer.setSpeed(1.08f);
 
 
             if (bufferSize != -1) {
